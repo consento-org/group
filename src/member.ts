20cb24fb3b23324a4662f3937abdb9eb35a60078
Martin Heidegger <martin.heidegger@gmail.com>
2020-12-22 15:23:32 +0900

    moved to eslint & standard

--- src/member.ts
+++ src/member.ts
@@ -1,394 +1,397 @@
-import { randomBytes } from "crypto";
-import { EventEmitter } from "events";
+import { randomBytes } from 'crypto'
+import { EventEmitter } from 'events'
 
-export type ResponseType = "accept" | "deny" | "cancel" | "conflict";
+export type ResponseType = 'accept' | 'deny' | 'cancel' | 'conflict'
 export type ResponseState =
-  | "pending"
-  | "ready"
-  | "cancelled"
-  | "failed"
-  | "finished";
-export type Operation = "add" | "remove" | "merge";
-export type RequestIdentifier = "request";
-export type ResponseIdentifier = "response";
-export type FeedItem = Request | Response;
+  | 'pending'
+  | 'ready'
+  | 'cancelled'
+  | 'failed'
+  | 'finished'
+export type Operation = 'add' | 'remove' | 'merge'
+export type RequestIdentifier = 'request'
+export type ResponseIdentifier = 'response'
+export type FeedItem = Request | Response
 
-export type ID = string;
+export type ID = string
 
-export const REQUEST_TYPE = "request" as RequestIdentifier;
-export const RESPONSE_TYPE = "response" as ResponseIdentifier;
+export const REQUEST_TYPE = 'request' as RequestIdentifier
+export const RESPONSE_TYPE = 'response' as ResponseIdentifier
 
 interface RawRequest {
   // Used to differentiate between req/res
-  type: RequestIdentifier;
+  type: RequestIdentifier
   // ID of the request
-  id: ID;
+  id: ID
   // ID of the creator
-  from: ID;
+  from: ID
   // What sort of operation this is
-  operation: Operation;
+  operation: Operation
 }
 
 export interface WhoRequest extends RawRequest {
   // Who to add or remove
-  who: ID;
+  who: ID
 }
 
 export interface MergeRequest extends RawRequest {
   // Which requests have been merged for this one
-  toMerge: Request[];
+  toMerge: Request[]
 }
 
-export type Request = WhoRequest | MergeRequest;
+export type Request = WhoRequest | MergeRequest
 
 export interface Response {
   // Used to differentiate between req/res
-  type: ResponseIdentifier;
+  type: ResponseIdentifier
   // ID of the request
-  id: ID;
+  id: ID
   // ID of the request creator
-  from: ID;
+  from: ID
   // Our response to this request
-  response: ResponseType;
+  response: ResponseType
 }
 
 export class RequestState {
-  req: Request;
+  req: Request
   signatures: {
     [id in ID]: Response;
-  };
-  finished: boolean;
-  lastState: ResponseState;
-
-  constructor(req: Request) {
-    this.req = req;
-    this.signatures = {};
-    this.finished = false;
-    this.lastState = "pending";
   }
 
-  isSignedBy(id: ID): boolean {
-    return !!this.signatures[id];
+  finished: boolean
+  lastState: ResponseState
+
+  constructor (req: Request) {
+    this.req = req
+    this.signatures = {}
+    this.finished = false
+    this.lastState = 'pending'
   }
 
-  calculateState(
+  isSignedBy (id: ID): boolean {
+    return this.signatures[id] !== undefined
+  }
+
+  calculateState (
     neededSignatures: number,
     maxDenied: number = neededSignatures
   ): ResponseState {
-    let state: ResponseState = "pending";
-    if (this.finished) state = "finished";
-    if (this.isCancelled()) state = "cancelled";
-    if (this.isConflicted()) state = "failed";
-    if (this.numberAccepted() >= neededSignatures) state = "ready";
-    if (this.numberDenied() > maxDenied) state = "failed";
+    let state: ResponseState = 'pending'
+    if (this.finished) state = 'finished'
+    if (this.isCancelled()) state = 'cancelled'
+    if (this.isConflicted()) state = 'failed'
+    if (this.numberAccepted() >= neededSignatures) state = 'ready'
+    if (this.numberDenied() > maxDenied) state = 'failed'
 
-    this.lastState = state;
+    this.lastState = state
 
-    return state;
+    return state
   }
 
-  addResponse(author: ID, response: Response) {
-    this.signatures[author] = response;
+  addResponse (author: ID, response: Response): void {
+    this.signatures[author] = response
   }
 
-  get id() {
-    return this.req.id;
+  get id (): ID {
+    return this.req.id
   }
 
-  get from() {
-    return this.req.from;
+  get from (): ID {
+    return this.req.from
   }
 
-  get operation() {
-    return this.req.operation;
+  get operation (): Operation {
+    return this.req.operation
   }
 
-  numberDenied(): number {
+  numberDenied (): number {
     return Object.keys(this.signatures).filter((id: ID) => {
-      return this.signatures[id].response === "deny";
-    }).length;
+      return this.signatures[id].response === 'deny'
+    }).length
   }
 
-  numberAccepted(): number {
+  numberAccepted (): number {
     return Object.keys(this.signatures).filter((id: ID) => {
-      return this.signatures[id].response === "accept";
-    }).length;
+      return this.signatures[id].response === 'accept'
+    }).length
   }
 
-  isConflicted(): boolean {
+  isConflicted (): boolean {
     return Object.keys(this.signatures).some((id: ID) => {
-      return this.signatures[id].response === "conflict";
-    });
+      return this.signatures[id].response === 'conflict'
+    })
   }
 
-  isCancelled(): boolean {
-    return this.signatures[this.from]?.response === "cancel";
+  isCancelled (): boolean {
+    return this.signatures[this.from]?.response === 'cancel'
   }
 
-  finish() {
-    this.finished = true;
+  finish (): void {
+    this.finished = true
   }
 }
 
-function allowAll(request: Request): boolean {
-  return true;
+function allowAll (_request: Request): boolean {
+  return true
 }
 
-export interface ShouldAcceptCB {
-  (request: Request): boolean;
-}
+export type ShouldAcceptCB = (request: Request) => boolean
 
 export interface MemberConstructorOptions {
-  id?: ID;
-  initiator?: ID;
-  shouldAccept?: ShouldAcceptCB;
+  id?: ID
+  initiator?: ID
+  shouldAccept?: ShouldAcceptCB
 }
 
 export class Member extends EventEmitter {
-  id: ID;
-  ownFeed: FeedItem[];
+  id: ID
+  ownFeed: FeedItem[]
   knownFeeds: {
     [id in ID]: FeedItem[];
-  };
-  memberIndexes: {
-    [id in ID]: number;
-  };
-  ownIndex: number;
-  requests: {
-    [id in ID]: RequestState;
-  };
-  knownMembers: string[];
-  shouldAccept: ShouldAcceptCB;
-
-  constructor(
-    { id, initiator, shouldAccept = allowAll }: MemberConstructorOptions = {
-      shouldAccept: allowAll,
-    }
-  ) {
-    super();
-    this.id = id || makeID();
-    this.ownFeed = [];
-    this.ownIndex = 0;
-    this.knownFeeds = {
-      [this.id]: this.ownFeed,
-    };
-    this.memberIndexes = {
-      [this.id]: 0,
-    };
-    this.requests = {};
-    this.knownMembers = [initiator || this.id];
-    this.shouldAccept = shouldAccept;
   }
 
-  sync(member: Member) {
+  memberIndexes: {
+    [id in ID]: number;
+  }
+
+  ownIndex: number
+  requests: {
+    [id in ID]: RequestState;
+  }
+
+  knownMembers: string[]
+  shouldAccept: ShouldAcceptCB
+
+  constructor (
+    { id, initiator, shouldAccept = allowAll }: MemberConstructorOptions = {
+      shouldAccept: allowAll
+    }
+  ) {
+    super()
+    this.id = id ?? makeID()
+    this.ownFeed = []
+    this.ownIndex = 0
+    this.knownFeeds = {
+      [this.id]: this.ownFeed
+    }
+    this.memberIndexes = {
+      [this.id]: 0
+    }
+    this.requests = {}
+    this.knownMembers = [initiator ?? this.id]
+    this.shouldAccept = shouldAccept
+  }
+
+  sync (member: Member): void {
     // Iterate through member's knownFeeds
     // Get any new data you don't have
     // Get new data from members to knownFeeds
     for (const id of Object.keys(member.knownFeeds)) {
-      const feed = member.knownFeeds[id];
-      const ownCopy = this.getFeedFor(id);
+      const feed = member.knownFeeds[id]
+      const ownCopy = this.getFeedFor(id)
       if (ownCopy.length < feed.length) {
-        this.updateFeedFor(id, feed);
+        this.updateFeedFor(id, feed)
       }
     }
 
     this.processFeeds()
   }
 
-  processFeeds(): boolean {
-    let hasProcessed = false;
-    let hasResponded = false;
+  processFeeds (): boolean {
+    let hasProcessed = false
+    const hasResponded = false
     for (const id of this.knownMembers) {
-      const index = this.getMemberIndexFor(id);
-      const feed = this.getFeedFor(id);
-      const item = feed[index];
-      if (!item) continue;
-      hasProcessed = true;
+      const index = this.getMemberIndexFor(id)
+      const feed = this.getFeedFor(id)
+      const item = feed[index]
+      if (item === undefined) continue
+      hasProcessed = true
 
       if (isRequest(item)) {
-        this.trackRequest(item);
+        this.trackRequest(item)
 
-        this.incrementMemberIndexFor(id);
+        this.incrementMemberIndexFor(id)
       } else if (isResponse(item)) {
         if (this.hasRequest(item.id)) {
-          const req = this.getRequest(item.id);
-          req.addResponse(id, item);
+          const req = this.getRequest(item.id)
+          req.addResponse(id, item)
 
-          let neededSignatures = this.knownMembers.length;
-          if (req.operation === "remove") neededSignatures--;
+          let neededSignatures = this.knownMembers.length
+          if (req.operation === 'remove') neededSignatures--
 
-          const maxDenied = this.knownMembers.length - neededSignatures;
+          const maxDenied = this.knownMembers.length - neededSignatures
 
-          const state = req.calculateState(neededSignatures, maxDenied);
+          const state = req.calculateState(neededSignatures, maxDenied)
 
-          if (state === "ready") {
-            if (req.operation === "add") {
+          if (state === 'ready') {
+            if (req.operation === 'add') {
               if (isWhoRequest(req.req)) {
-                const who = req.req.who;
-                this.knownMembers.push(who);
+                const who = req.req.who
+                this.knownMembers.push(who)
               }
-            } else if (req.operation === "remove") {
+            } else if (req.operation === 'remove') {
               if (isWhoRequest(req.req)) {
-                const who = req.req.who;
+                const who = req.req.who
                 this.knownMembers = this.knownMembers.filter(
                   (id) => id !== who
-                );
+                )
               }
             }
-            req.finish();
+            req.finish()
             if (this.isMember()) {
-              this.emit("block", req);
+              this.emit('block', req)
             }
           }
-          this.incrementMemberIndexFor(id);
+          this.incrementMemberIndexFor(id)
         } else {
-          continue;
+          continue
         }
       } else {
-        console.warn("Got invalid item", item);
-        this.incrementMemberIndexFor(id);
+        console.warn('Got invalid item', item)
+        this.incrementMemberIndexFor(id)
       }
     }
 
     if (hasProcessed) {
-      const otherRun = this.processFeeds();
-      return hasResponded || otherRun;
-    } else return hasResponded;
+      const otherRun = this.processFeeds()
+      return hasResponded || otherRun
+    } else return hasResponded
   }
 
-  private updateFeedFor(id: ID, feed: FeedItem[]) {
-    this.knownFeeds[id] = feed.slice(0);
+  private updateFeedFor (id: ID, feed: FeedItem[]): void {
+    this.knownFeeds[id] = feed.slice(0)
   }
 
-  private hasNewFeedItemsFor(id: ID): boolean {
-    return this.getMemberIndexFor(id) === this.getFeedFor(id).length - 1;
+  private hasNewFeedItemsFor (id: ID): boolean {
+    return this.getMemberIndexFor(id) === this.getFeedFor(id).length - 1
   }
 
-  private getMemberIndexFor(id: ID): number {
-    if (this.memberIndexes[id]) return this.memberIndexes[id];
-    this.memberIndexes[id] = 0;
-    return 0;
+  private getMemberIndexFor (id: ID): number {
+    if (this.memberIndexes[id] !== undefined) return this.memberIndexes[id]
+    this.memberIndexes[id] = 0
+    return 0
   }
 
-  private incrementMemberIndexFor(id: ID) {
-    this.memberIndexes[id] = this.getMemberIndexFor(id) + 1;
+  private incrementMemberIndexFor (id: ID): void {
+    this.memberIndexes[id] = this.getMemberIndexFor(id) + 1
   }
 
-  private getFeedFor(id: ID): FeedItem[] {
-    if (!this.knownFeeds[id]) {
-      this.knownFeeds[id] = [];
+  private getFeedFor (id: ID): FeedItem[] {
+    if (this.knownFeeds[id] === undefined) {
+      this.knownFeeds[id] = []
     }
 
-    return this.knownFeeds[id];
+    return this.knownFeeds[id]
   }
 
-  private hasRequest(id: ID): boolean {
-    return !!this.requests[id];
+  private hasRequest (id: ID): boolean {
+    return this.requests[id] !== undefined
   }
 
-  private trackRequest(request: Request) {
-    const { id } = request;
-    this.requests[id] = new RequestState(request);
+  private trackRequest (request: Request): void {
+    const { id } = request
+    this.requests[id] = new RequestState(request)
   }
 
-  private getRequest(id: ID): RequestState {
-    return this.requests[id];
+  private getRequest (id: ID): RequestState {
+    return this.requests[id]
   }
 
-  requestAdd(who: ID): Request {
-    return this.makeRequest(who, "add");
+  requestAdd (who: ID): Request {
+    return this.makeRequest(who, 'add')
   }
 
-  requestRemove(who: ID): Request {
-    return this.makeRequest(who, "remove");
+  requestRemove (who: ID): Request {
+    return this.makeRequest(who, 'remove')
   }
 
-  makeRequest(who: ID, operation: Operation): Request {
+  makeRequest (who: ID, operation: Operation): Request {
     const req = {
       type: REQUEST_TYPE,
       id: makeID(),
       from: this.id,
       who,
-      operation,
-    };
+      operation
+    }
 
-    this.ownFeed.push(req);
+    this.ownFeed.push(req)
 
     // Automatically accept requests you created
-    this.acceptRequest(req);
+    this.acceptRequest(req)
 
-    return req;
+    return req
   }
 
-  makeResponse(request: Request, response: ResponseType): Response {
-    const { id, from } = request;
+  makeResponse (request: Request, response: ResponseType): Response {
+    const { id, from } = request
     const res = {
       type: RESPONSE_TYPE,
       from,
       id,
-      response,
-    };
+      response
+    }
 
-    this.ownFeed.push(res);
+    this.ownFeed.push(res)
 
-    return res;
+    return res
   }
 
-  acceptRequest(request: Request): Response {
-    return this.makeResponse(request, "accept");
+  acceptRequest (request: Request): Response {
+    return this.makeResponse(request, 'accept')
   }
 
-  denyRequest(request: Request): Response {
-    return this.makeResponse(request, "deny");
+  denyRequest (request: Request): Response {
+    return this.makeResponse(request, 'deny')
   }
 
-  getPendingRequests(): RequestState[] {
-    const pending = [];
+  getPendingRequests (): RequestState[] {
+    const pending = []
     // Down the line we'll want to put finished requests somewhere else
-    for (let id of Object.keys(this.requests)) {
-      const request = this.requests[id];
-      const state = request.lastState;
-      if (state !== "pending") continue;
-      if (request.isSignedBy(this.id)) continue;
-      pending.push(request);
+    for (const id of Object.keys(this.requests)) {
+      const request = this.requests[id]
+      const state = request.lastState
+      if (state !== 'pending') continue
+      if (request.isSignedBy(this.id)) continue
+      pending.push(request)
     }
 
-    return pending;
+    return pending
   }
 
-  acceptPending(): number {
-    let accepted = 0;
-    for (let requestState of this.getPendingRequests()) {
-      this.acceptRequest(requestState.req);
-      accepted++;
+  acceptPending (): number {
+    let accepted = 0
+    for (const requestState of this.getPendingRequests()) {
+      this.acceptRequest(requestState.req)
+      accepted++
     }
 
-    this.processFeeds();
+    this.processFeeds()
 
-    return accepted;
+    return accepted
   }
 
-  isMember() {
-    return this.knownMembers.includes(this.id);
+  isMember (): boolean {
+    return this.knownMembers.includes(this.id)
   }
 }
 
-export function isRequest(item: FeedItem): item is Request {
-  return item.type === REQUEST_TYPE;
+export function isRequest (item: FeedItem): item is Request {
+  return item.type === REQUEST_TYPE
 }
 
-export function isResponse(item: FeedItem): item is Response {
-  return item.type === RESPONSE_TYPE;
+export function isResponse (item: FeedItem): item is Response {
+  return item.type === RESPONSE_TYPE
 }
 
-function isMergeRequest(item: Request): item is MergeRequest {
-  return (item as MergeRequest).toMerge !== undefined;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function isMergeRequest (item: Request): item is MergeRequest {
+  return (item as MergeRequest).toMerge !== undefined
 }
 
-function isWhoRequest(item: Request): item is WhoRequest {
-  return (item as WhoRequest).who !== undefined;
+function isWhoRequest (item: Request): item is WhoRequest {
+  return (item as WhoRequest).who !== undefined
 }
 
-function makeID(): ID {
-  return randomBytes(8).toString("hex");
+function makeID (): ID {
+  return randomBytes(8).toString('hex')
 }
--- test/index.ts
+++ test/index.ts
@@ -1,150 +1,150 @@
-import test from "fresh-tape";
+import test from 'fresh-tape'
 
-import { Member } from "../src/member";
+import { Member } from '../src/member'
 
-const EXAMPLE_ID = "example";
-const EXAMPLE_ID_2 = "example2";
+const EXAMPLE_ID = 'example'
 
-test("Able to initialize a member", (t) => {
-  const member = new Member();
+test('Able to initialize a member', (t) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const member = new Member()
 
-  t.pass("Able to process feeds with zero data");
+  t.pass('Able to process feeds with zero data')
 
-  t.end();
-});
+  t.end()
+})
 
-test("Able to add a member by ID", (t) => {
-  const member = new Member();
+test('Able to add a member by ID', (t) => {
+  const member = new Member()
 
-  const req = member.requestAdd(EXAMPLE_ID);
+  const req = member.requestAdd(EXAMPLE_ID)
 
-  t.ok(req, "Generated a request");
+  t.ok(req, 'Generated a request')
 
-  t.equal(member.ownFeed[1].type, "response", "auto-generated response")
+  t.equal(member.ownFeed[1].type, 'response', 'auto-generated response')
 
-  member.processFeeds();
+  member.processFeeds()
 
-  t.pass("Processed feeds");
+  t.pass('Processed feeds')
 
-  const expectedMembers = [member.id, EXAMPLE_ID];
+  const expectedMembers = [member.id, EXAMPLE_ID]
   t.deepEqual(
     member.knownMembers,
     expectedMembers,
-    "New member got added to list"
-  );
+    'New member got added to list'
+  )
 
-  t.end();
-});
+  t.end()
+})
 
-test("Able to add a member by ID and sync", (t) => {
-  const member = new Member();
-  const other = new Member({ initiator: member.id });
+test('Able to add a member by ID and sync', (t) => {
+  const member = new Member()
+  const other = new Member({ initiator: member.id })
 
-  const req = member.requestAdd(other.id);
+  const req = member.requestAdd(other.id)
 
-  t.ok(req, "Generated a request");
+  t.ok(req, 'Generated a request')
 
-  const res = member.acceptRequest(req);
+  const res = member.acceptRequest(req)
 
-  t.ok(res, "Generated response");
+  t.ok(res, 'Generated response')
 
-  member.processFeeds();
+  member.processFeeds()
 
-  other.sync(member);
+  other.sync(member)
 
-  t.pass("Able to sync with member");
+  t.pass('Able to sync with member')
 
-  other.processFeeds();
+  other.processFeeds()
 
-  t.pass("Able to process feeds after sync");
+  t.pass('Able to process feeds after sync')
 
-  const expectedMembers = [member.id, other.id];
+  const expectedMembers = [member.id, other.id]
   t.deepEqual(
     other.knownMembers,
     expectedMembers,
-    "New member saw itself added to list"
-  );
+    'New member saw itself added to list'
+  )
 
-  t.end();
-});
+  t.end()
+})
 
-test("Process request by syncing one peer at a time", (t) => {
-  const members = initializeMembers(5);
+test('Process request by syncing one peer at a time', (t) => {
+  const members = initializeMembers(5)
 
-  let previous = members[0];
-  previous.requestAdd(EXAMPLE_ID);
-  previous.processFeeds();
+  let previous = members[0]
+  previous.requestAdd(EXAMPLE_ID)
+  previous.processFeeds()
 
-  for (let next of members.slice(1)) {
-    sync(previous, next);
-    next.acceptPending();
-    sync(previous, next);
-    previous = next;
+  for (const next of members.slice(1)) {
+    sync(previous, next)
+    next.acceptPending()
+    sync(previous, next)
+    previous = next
   }
 
-  const wasAdded = previous.knownMembers.includes(EXAMPLE_ID);
-  t.ok(wasAdded, "Request got processed successfully");
+  const wasAdded = previous.knownMembers.includes(EXAMPLE_ID)
+  t.ok(wasAdded, 'Request got processed successfully')
 
-  t.end();
-});
+  t.end()
+})
 
-test("Two members do an add at once", (t) => {
-	const members = initializeMembers(5)
+test('Two members do an add at once', (t) => {
+  const members = initializeMembers(5)
 
-	const [a,b,c,d,e] = members
+  const [a, b, c, d, e] = members
 
-	const f = new Member()
-	const g = new Member()
-	f.knownMembers = a.knownMembers.slice()
-	g.knownMembers = a.knownMembers.slice()
+  const f = new Member()
+  const g = new Member()
+  f.knownMembers = a.knownMembers.slice()
+  g.knownMembers = a.knownMembers.slice()
 
-  const requestA = a.requestAdd(f.id)
+  a.requestAdd(f.id)
 
-  const requestD = d.requestAdd(g.id)
+  d.requestAdd(g.id)
 
-  sync(a,b)
+  sync(a, b)
 
-  t.equal(b.acceptPending(), 1, "B accepted Request A")
+  t.equal(b.acceptPending(), 1, 'B accepted Request A')
 
-  sync(d,e)
+  sync(d, e)
 
-  t.equal(e.acceptPending(), 1, "E accepted Request D")
+  t.equal(e.acceptPending(), 1, 'E accepted Request D')
 
   sync(e, c)
   sync(b, c)
 
   const pending = c.getPendingRequests()
 
-  t.equal(pending.length, 2, "C sees 2 pending requests")
+  t.equal(pending.length, 2, 'C sees 2 pending requests')
 
   sync(c, b)
 
-  t.equal(b.acceptPending(), 1, "B accepted Request D")
+  t.equal(b.acceptPending(), 1, 'B accepted Request D')
 
   sync(c, e)
 
-  t.equal(e.acceptPending(), 1, "E accepted Request A")
+  t.equal(e.acceptPending(), 1, 'E accepted Request A')
 
   sync(e, d)
 
-  t.equal(d.acceptPending(), 1, "D accepted Request A")
+  t.equal(d.acceptPending(), 1, 'D accepted Request A')
 
   sync(b, a)
 
-  t.equal(a.acceptPending(), 1, "A accepted request D")
+  t.equal(a.acceptPending(), 1, 'A accepted request D')
 
   sync(a, c)
   sync(d, c)
 
   const ready = c.getPendingRequests()
 
-  t.equal(ready.length, 2, "C sees 2 pending requests")
+  t.equal(ready.length, 2, 'C sees 2 pending requests')
 
   c.acceptPending()
 
-  const wasAddedA = c.knownMembers.includes(f.id);
+  const wasAddedA = c.knownMembers.includes(f.id)
 
-  const wasAddedD = c.knownMembers.includes(g.id);
+  const wasAddedD = c.knownMembers.includes(g.id)
 
   t.notOk(wasAddedA, 'F was not added via A')
   t.ok(wasAddedD, 'G was added via D')
@@ -153,9 +153,9 @@ test("Two members do an add at once", (t) => {
 
   const finallyPending = g.getPendingRequests()
 
-  t.equal(finallyPending.length, 1, "G sees 1 pending request")
+  t.equal(finallyPending.length, 1, 'G sees 1 pending request')
 
-  t.equal(g.acceptPending(), 1, "G accepted request A")
+  t.equal(g.acceptPending(), 1, 'G accepted request A')
 
   const wasFinallyAddedA = g.knownMembers.includes(f.id)
 
@@ -165,21 +165,20 @@ test("Two members do an add at once", (t) => {
 })
 
 test('Happy path of adding several members together', (t) => {
-  const a = new Member({id: 'a'})
-  const b = new Member({id: 'b', initiator: 'a'})
-  const c = new Member({id: 'c', initiator: 'a'})
-  const d = new Member({id: 'd', initiator: 'a'})
-  const e = new Member({id: 'e', initiator: 'a'})
+  const a = new Member({ id: 'a' })
+  const b = new Member({ id: 'b', initiator: 'a' })
+  const c = new Member({ id: 'c', initiator: 'a' })
+  const d = new Member({ id: 'd', initiator: 'a' })
+  const e = new Member({ id: 'e', initiator: 'a' })
 
   const currentMembers = [a]
-  const allMembers = [a,b,c,d,e]
 
   authorizeMember(b)
   authorizeMember(c)
   authorizeMember(d)
   authorizeMember(e)
 
-  const f = new Member({id: 'f', initiator: 'a'})
+  const f = new Member({ id: 'f', initiator: 'a' })
 
   sync(f, c)
 
@@ -187,55 +186,55 @@ test('Happy path of adding several members together', (t) => {
 
   t.end()
 
-  function authorizeMember(member: Member) {
-		const initiator = currentMembers[currentMembers.length -1]
-		const others = currentMembers.slice(0, -1)
+  function authorizeMember (member: Member): void {
+    const initiator = currentMembers[currentMembers.length - 1]
+    const others = currentMembers.slice(0, -1)
 
-		initiator.requestAdd(member.id)
+    initiator.requestAdd(member.id)
 
-		let previous = initiator
+    let previous = initiator
 
-		for(let next of others) {
-			sync(previous, next)
+    for (const next of others) {
+      sync(previous, next)
 
-			const pending = next.getPendingRequests()
+      const pending = next.getPendingRequests()
 
-			t.equal(pending.length, 1, `${next.id} sees pending request ${member.id}`)
+      t.equal(pending.length, 1, `${next.id} sees pending request ${member.id}`)
 
-			const accepted = next.acceptPending()
+      const accepted = next.acceptPending()
 
-			t.equal(accepted, 1, `${next.id} acepted pending request ${member.id}`)
+      t.equal(accepted, 1, `${next.id} acepted pending request ${member.id}`)
 
-			previous = next
-		}
+      previous = next
+    }
 
-		const exists  = previous.knownMembers.includes(member.id)
+    const exists = previous.knownMembers.includes(member.id)
 
-		t.ok(exists, 'Member got added')
+    t.ok(exists, 'Member got added')
 
-		currentMembers.push(member)
+    currentMembers.push(member)
 
-		for(let next of currentMembers) {
-       sync(next, previous)
+    for (const next of currentMembers) {
+      sync(next, previous)
 
-       t.deepEquals(next.knownMembers, previous.knownMembers, `${next.id} resolved expected members`)
-		}
+      t.deepEquals(next.knownMembers, previous.knownMembers, `${next.id} resolved expected members`)
+    }
   }
 })
 
-function sync(member1: Member, member2: Member) {
-  member1.sync(member2);
-  member2.sync(member1);
+function sync (member1: Member, member2: Member): void {
+  member1.sync(member2)
+  member2.sync(member1)
 }
 
-function initializeMembers(n: number) {
-  const members: Member[] = [];
-  while (n--) members.push(new Member());
+function initializeMembers (n: number): Member[] {
+  const members: Member[] = []
+  while (n-- > 0) members.push(new Member())
 
-  const knownMembers = members.map(({ id }: Member) => id);
-  for (let member of members) {
-    member.knownMembers = knownMembers;
+  const knownMembers = members.map(({ id }: Member) => id)
+  for (const member of members) {
+    member.knownMembers = knownMembers
   }
 
-  return members;
+  return members
 }
