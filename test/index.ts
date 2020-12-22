f37be83ed910f4da7c182c765987b27dafa6181d
mauve <ranger@mauve.moe>
2020-12-22 02:16:34 -0500

    Make linter happy

--- test/index.ts
+++ test/index.ts
@@ -165,7 +165,7 @@ test('Two members do an add at once', (t) => {
 })
 
 test('Happy path of adding several members together', (t) => {
-  const a = new Member({ id: 'a'})
+  const a = new Member({ id: 'a' })
   const b = new Member({ id: 'b', initiator: 'a' })
   const c = new Member({ id: 'c', initiator: 'a' })
   const d = new Member({ id: 'd', initiator: 'a' })
@@ -194,22 +194,24 @@ test('Happy path of adding several members together', (t) => {
 
     let previous = initiator
 
-    if(!others.length) {
+    if (others.length === 0) {
       const pending = initiator.getPendingRequests()
 
       t.equal(pending.length, 0, `${initiator.id} doesn't see pending request ${member.id}`)
-    } else for (const next of others) {
-      sync(previous, next)
+    } else {
+      for (const next of others) {
+        sync(previous, next)
 
-      const pending = next.getPendingRequests()
+        const pending = next.getPendingRequests()
 
-      t.equal(pending.length, 1, `${next.id} sees pending request ${member.id}`)
+        t.equal(pending.length, 1, `${next.id} sees pending request ${member.id}`)
 
-      const accepted = next.acceptPending()
+        const accepted = next.acceptPending()
 
-      t.equal(accepted, 1, `${next.id} acepted pending request ${member.id}`)
+        t.equal(accepted, 1, `${next.id} acepted pending request ${member.id}`)
 
-      previous = next
+        previous = next
+      }
     }
 
     const exists = previous.knownMembers.includes(member.id)
